ced16bedb8a4b8aabf24b31ab27fc5d64d20844f
Kristian Øllegaard <kristian@kristian.io>
2012-04-30 01:32:27 +0200

    Refacter to do caching manually, as t/l was faster 1.7 only

--- lib/facter/country.rb
+++ lib/facter/country.rb
@@ -2,15 +2,24 @@ require 'rubygems'
 require 'json'
 require 'net/http'
 
-def ip_search(ip)
-   url = "http://stat.ripe.net/plugin/geoloc/data.json?resource=#{ip}"
-   resp = Net::HTTP.get_response(URI.parse(url))
-   result = JSON.parse(resp.body)
-   return result['data']['locations'].first
+def ip_search()
+   ip = Facter.value('ipaddress')
+   data_file = "/var/lib/puppet/state/fact_geoloc.json"
+   if File.exists?(data_file):
+     data = JSON.parse(File.open(data_file).read)
+   else
+     url = "http://stat.ripe.net/plugin/geoloc/data.json?resource=#{ip}"
+     resp = Net::HTTP.get_response(URI.parse(url))
+     result = JSON.parse(resp.body)
+     data = result['data']['locations'].first
+     dest = File.new(data_file, 'w+')
+     dest.puts(data.to_json)
+   end
+   return 
 end
 
-Facter.add("country", :ttl => 2629743) do # One month TTL
+Facter.add("country") do
     setcode do
-      ip_search(Facter.value('ipaddress'))["country"]
+      return ip_search["country"]
     end
 end
